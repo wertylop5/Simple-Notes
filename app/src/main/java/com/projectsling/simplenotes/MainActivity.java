392a09ed5247a8866a9630b1fc890cbc7cbadad3
Stanley Lin <slin55055@gmail.com>
2017-04-13 18:55:58 -0400

    ListView for notes works

--- app/src/main/java/com/projectsling/simplenotes/MainActivity.java
+++ app/src/main/java/com/projectsling/simplenotes/MainActivity.java
@@ -1,6 +1,7 @@
 package com.projectsling.simplenotes;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,19 +17,76 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private static final String NOTE_LIST_TAG = "noteList";
+
     private Button mCreateButton;
     private ListView mNoteList;
     private List<JSONObject> mNotes;
+
+
+    private class ReadNoteFileTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            BufferedReader file = null;
+            try {
+                file = new BufferedReader(
+                        new InputStreamReader(openFileInput(getString(R.string.note_file))));
+                Log.v(TAG, file.toString());
+
+                //print all notes in file
+                JSONObject note;
+                String line;
+                while((line = file.readLine()) != null) {
+                    note = new JSONObject(line);
+                    Log.v(TAG, note.toString(4));
+                    mNotes.add(note);
+                }
+
+            } catch (FileNotFoundException e) {
+                Log.e(TAG, "FileNotFoundException", e);
+            } catch (IOException e) {
+                Log.e(TAG, "IOException", e);
+            } catch (JSONException e) {
+                Log.e(TAG, "JSONException", e);
+            }
+            finally {
+                try {
+                    if (file != null) {
+                        file.close();
+                        Log.v(TAG, "File closed");
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "IOException", e);
+                }
+            }
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            mNotes = new ArrayList<>();
+
+            new ReadNoteFileTask().execute();
+        }
+        else {
+            Log.v(TAG, "onCreate: restore bundle");
+            mNotes = stringListToJson(savedInstanceState.getStringArrayList(NOTE_LIST_TAG));
+        }
+
+        //for (int x = 0; x < 30; x++) mNotes.add(null);
+
+        //mNotes.add(null);
 
         mCreateButton = (Button) findViewById(R.id.createNoteButton);
         mCreateButton.setOnClickListener(new View.OnClickListener() {
@@ -46,37 +104,44 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+    }
 
-        BufferedReader file = null;
-        try {
-            file = new BufferedReader(
-                    new InputStreamReader(openFileInput(getString(R.string.note_file))));
-            Log.v(TAG, file.toString());
-
-            //print all notes in file
-            JSONObject note;
-            String line;
-            while((line = file.readLine()) != null) {
-                note = new JSONObject(line);
-                Log.v(TAG, note.toString(4));
-            }
-
-        } catch (FileNotFoundException e) {
-            Log.e(TAG, "FileNotFoundException", e);
-        } catch (IOException e) {
-            Log.e(TAG, "IOException", e);
-        } catch (JSONException e) {
-            Log.e(TAG, "JSONException", e);
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        if (mNotes == null) {
+            Log.v(TAG, "onRestore");
+            mNotes = stringListToJson(savedInstanceState.getStringArrayList(NOTE_LIST_TAG));
         }
-        finally {
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putStringArrayList(NOTE_LIST_TAG, jsonListToString(mNotes));
+    }
+
+    //For use with bundles
+    private ArrayList<String> jsonListToString(List<JSONObject> l) {
+        ArrayList<String> res = new ArrayList<>();
+
+        for (JSONObject j : l) {
+            res.add(j.toString());
+        }
+
+        return res;
+    }
+
+    //For use with bundles
+    private ArrayList<JSONObject> stringListToJson(List<String> l) {
+        ArrayList<JSONObject> res = new ArrayList<>();
+
+        for (String s : l) {
             try {
-                if (file != null) {
-                    file.close();
-                    Log.v(TAG, "File closed");
-                }
-            } catch (IOException e) {
-                Log.e(TAG, "IOException", e);
+                res.add(new JSONObject(s));
+            } catch (JSONException e) {
+                Log.e(TAG, "JSONException", e);
             }
         }
+
+        return res;
     }
 }
--- app/src/main/java/com/projectsling/simplenotes/NoteAdapter.java
+++ app/src/main/java/com/projectsling/simplenotes/NoteAdapter.java
@@ -1,10 +1,14 @@
 package com.projectsling.simplenotes;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.List;
@@ -13,12 +17,19 @@ import java.util.List;
  * Created by Stanley on 4/12/2017.
  */
 public class NoteAdapter extends BaseAdapter {
+    private static final String TAG = NoteAdapter.class.getSimpleName();
+
     private Context mContext;
     private List<JSONObject> mNotes;
 
     public NoteAdapter(Context c, List<JSONObject> l) {
         mContext = c;
         mNotes = l;
+    }
+
+    private class ViewHolder {
+        public TextView mTitleText;
+        public TextView mBodyText;
     }
 
     @Override
@@ -39,7 +50,34 @@ public class NoteAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        ViewHolder holder;
+        if (convertView == null) {
+            //equivalent to context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)
+            LayoutInflater inflater = LayoutInflater.from(mContext);
 
-        return null;
+            convertView = inflater.inflate(R.layout.row_note_adapter, parent, false);
+            holder = new ViewHolder();
+            holder.mTitleText = (TextView)convertView.findViewById(R.id.noteTitle);
+            holder.mBodyText = (TextView)convertView.findViewById(R.id.noteBody);
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder)convertView.getTag();
+        }
+
+        try {
+            holder.mTitleText.setText(
+                    mNotes.get(position).getString(mContext.getString(R.string.note_title))
+            );
+
+            holder.mBodyText.setText(
+                    mNotes.get(position).getString(mContext.getString(R.string.note_body))
+            );
+        } catch (JSONException e) {
+            Log.e(TAG, "JSONException", e);
+        }
+
+
+        return convertView;
     }
 }
